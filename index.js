9baf2da048a65945e51a25ceaad84460af05ac91
SumitNalavade <vs.nalavade2003@gmail.com>
2021-12-25 12:55:12 -0600

    change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 22;
 
 const { getGPA } = require("./puppeteerFunctions");
 
