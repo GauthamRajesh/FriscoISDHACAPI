7893e82211c53a5fc9a9ef1ab52cc57a3d9398ff
SumitNalavade <vs.nalavade2003@gmail.com>
2021-12-25 18:17:39 -0600

    add student info route

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
-const { getGPA } = require("./puppeteerFunctions");
+const { getGPA, getInfo } = require("./puppeteerFunctions");
 
 app.listen(PORT, (req, res) => {
     console.log(`Express app listening on port ${PORT}`);
@@ -21,4 +21,14 @@ app.get("/students/gpa", async (req, res) => {
     })
     
     return res.send(gpas);
+})
+
+app.get("/students/info", async (req, res) => {
+    const { username, password } = req.query;
+
+    const info = await getInfo(username, password).catch((error) => {
+        console.log(error);
+    })
+    
+    return res.send(info);
 })
--- puppeteerFunctions.js
+++ puppeteerFunctions.js
@@ -25,22 +25,55 @@ exports.getGPA = async function(username, password) {
 
     const { page, browser } = await startBrowser(username, password);
 
-    try {
-        await page.goto("https://hac.friscoisd.org/HomeAccess/Content/Student/Transcript.aspx")
+    
+    await page.goto("https://hac.friscoisd.org/HomeAccess/Content/Student/Transcript.aspx")
         let elm = await page.$("#plnMain_rpTranscriptGroup_lblGPACum1")
         weightedGPA = await (await elm.getProperty('textContent')).jsonValue()
         
         elm = await page.$("#plnMain_rpTranscriptGroup_lblGPACum2")
         unweightedGPA = await (await elm.getProperty('textContent')).jsonValue()
-    } catch(e) {
-        console.log(e);
-    } finally {
-        await browser.close()
-
-    }
     
+    
+    await browser.close()
+
     return {
         weightedGPA: weightedGPA,
         unweightedGPA: unweightedGPA
     };
+}
+
+exports.getInfo = async function(username, password) {
+   let studentID, studentName, studentBirthDate, studentCounselor, studentBuilding, studentGrade;
+
+   const { page, browser } = await startBrowser(username, password);
+
+   await page.goto("https://hac.friscoisd.org/HomeAccess/Content/Student/Registration.aspx")
+   let elm = await page.$("#plnMain_lblRegStudentID")
+   studentID = await (await elm.getProperty('textContent')).jsonValue()
+   
+   elm = await page.$("#plnMain_lblRegStudentName")
+   studentName = await (await elm.getProperty('textContent')).jsonValue()
+
+   elm = await page.$("#plnMain_lblBirthDate")
+   studentBirthDate = await (await elm.getProperty('textContent')).jsonValue()
+
+   elm = await page.$("#plnMain_lblCounselor")
+   studentCounselor = await (await elm.getProperty('textContent')).jsonValue()
+
+   elm = await page.$("#plnMain_lblBuildingName")
+   studentBuilding = await (await elm.getProperty('textContent')).jsonValue()
+
+   elm = await page.$("#plnMain_lblGrade")
+   studentGrade = await (await elm.getProperty('textContent')).jsonValue()
+
+    await browser.close()
+
+    return {
+        studentID,
+        studentName,
+        studentBirthDate,
+        studentCounselor,
+        studentBuilding,
+        studentGrade
+    }
 }
