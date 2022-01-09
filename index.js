6214cfc11a69f80e2a4e55cafb9ba8b735d4bce3
SumitNalavade <vs.nalavade2003@gmail.com>
2022-01-09 14:10:03 -0600

    Add fake data middleware

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const cors = require("cors");
 const fakeData = require("./fakeData");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT || 3000;
+app.all("/students/*", (req, res, next) => {
+    const { username, password } = req.query;
+
+    if(username.toLowerCase() === "john" && password.toLowerCase() === "doe") {
+        return res.send(fakeData);
+    } else {
+        return next();
+    }
+});
 
 const { getGPA, getInfo, getClasses, getClassesDetails, getPredictedGPA } = require("./puppeteerFunctions");
 
@@ -20,44 +29,38 @@ app.get("/", (req, res) => {
 
 app.get("/students/gpa", async (req, res) => {
     const { username, password } = req.query;
-
-    if(username.toLowerCase() === "john" && password.toLowerCase() === "doe") {
-        return res.send(fakeData.currentGPAS);
-    }
     
     const gpas = await getGPA(username, password).catch((error) => {
         console.log(error);
     })
     
-    return res.send(gpas);
+    return res.send({
+        currentGPAS: gpas
+    });
 })
 
 app.get("/students/info", async (req, res) => {
     const { username, password } = req.query;
 
-    if(username.toLowerCase() === "john" && password.toLowerCase() === "doe") {
-        return res.send(fakeData.studentData);
-    }
-
     const info = await getInfo(username, password).catch((error) => {
         console.log(error);
     })
     
-    return res.send(info);
+    return res.send({
+        studentData: info
+    });
 })
 
 app.get("/students/currentclasses", async (req, res) => {
     const { username, password } = req.query;
 
-    if(username.toLowerCase() === "john" && password.toLowerCase() === "doe") {
-        return res.send(fakeData.currentClasses);
-    }
-
     const classes = await getClasses(username, password).catch((error) => {
         console.log(error);
     })
 
-    res.send(classes);
+    return res.send({
+        currentClasses: classes
+    });
 })
 
 app.get("/students/currentclasses/details", async (req, res) => {
@@ -67,10 +70,12 @@ app.get("/students/currentclasses/details", async (req, res) => {
         console.log(error);
     })
 
-    res.send(classes);
+    res.send({
+        currentClassesDetails: classes
+    });
 });
 
-app.post("/students/predictedGPA", (req, res) => {
+app.post("/predictedGPA", (req, res) => {
     const { weightedGPA, unweightedGPA, currentClasses, studentGrade } = req.body;
 
     const predictedGPAs = getPredictedGPA(weightedGPA, unweightedGPA, currentClasses, studentGrade)
